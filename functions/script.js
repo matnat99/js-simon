250dbdb3bb6fd053ed6d056d00473dd66d38d4cb
mattianatale <mattianatale07@gmail.com>
2024-12-12 17:15:27 +0100

    fix: corrections

---
+++ functions/script.js
@@ -0,0 +1,4 @@
+//FUNCTIONS
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+}
--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-//FUNCTIONS
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
-
 //DOM ELEMENTS
 //Forms
 const formElm = document.getElementById('form')
@@ -10,21 +5,27 @@ const formElm = document.getElementById('form')
 const timerElm = document.getElementById('timer')
 const pElm = document.getElementById('p')
 const randomGroupElm = document.getElementById('random-group')
-const randomElm = document.getElementsByClassName('random')
+const randomElm = document.querySelectorAll('.random')
 const inputGroupElm = document.getElementById('input-group')
 const submitterElm = document.getElementById('submitter')
 //Form Fields
 const userInputsElm = document.getElementsByClassName('user-imputs')
 
 //DOM Events
-/* Visualizzare in pagina 5 numeri casuali */
+// Visualizzare in pagina 5 numeri casuali
 const randomNumbers = []
 
-for (let i = 0; i < randomElm.length; i++) {
+while(randomNumbers.length < 5){
     const randomValue = getRndInteger(1, 100)
-    randomElm[i].innerHTML = randomValue
-    randomNumbers.push(randomValue)
-  }
+    if(!randomNumbers.includes(randomValue)){
+        randomNumbers.push(randomValue)
+    }
+}
+
+for(let i = 0; i < randomNumbers.length; i++){
+    randomElm[i].innerHTML = randomNumbers[i]
+}
+
 
 /* Partenza timer */
 let seconds = 30
@@ -43,30 +44,25 @@ const intervalID = setInterval(() =>{
     seconds--
 }, 1000)
 
-/* Estrarre i valori dagli imput utente */ 
-submitterElm.addEventListener('click',(event) =>{
+/* Estrarre i valori dagli input utente */ 
+submitterElm.addEventListener('click', (event) =>{
     event.preventDefault()
     const userNumbers = []
-    let isValid = true
 
     for (let i = 0; i < userInputsElm.length; i++) {
-        const userNumbervalue = parseInt(userInputsElm[i].value)
-        if (!isNaN(userNumbervalue)) { 
-            userNumbers.push(userNumbervalue)
+        const userNumberValue = parseInt(userInputsElm[i].value)
+        if (!isNaN(userNumberValue) && !userNumbers.includes(userNumberValue)) { 
+            userNumbers.push(userNumberValue)
         }
     }
 
     /* Confrontare quali e quanti numeri corrispondono */
-    const correctNumbers = [];
+    const correctNumbers = []
     for (let i = 0; i < userNumbers.length; i++) {
-        if (randomNumbers.includes(userNumbers[i])) {
+        if (randomNumbers.includes(userNumbers[i]) && !correctNumbers.includes(userNumbers[i])) {
             correctNumbers.push(userNumbers[i])
         }
-}
-const resultElm = document.getElementById('result')
-resultElm.innerHTML = `Hai indovinato ${correctNumbers.length} numeri! (${correctNumbers.join(', ')})`
+    }
+    const resultElm = document.getElementById('result')
+    resultElm.innerHTML = `Hai indovinato ${correctNumbers.length} numeri! (${correctNumbers.join(', ')})`
 })
-
-
-
-
